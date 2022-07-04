727e877455245caea5c54cda676a530c23ecbd84
Ronald Abu Saleh <lancerrnld@gmail.com>
2022-07-03 20:04:22 -0400

    Login and UI components index changes

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,10 +1,11 @@
+import { Input, Button, FormGroup, Label, H1 } from "../../components/ui";
 import { useState } from "react";
-import { Link } from "react-router-dom";
-import { Input, Button, FormGroup, H1 } from "../../components/ui";
+import { Link, useNavigate } from "react-router-dom";
+
 import { MdKeyboardArrowRight } from "react-icons/md";
+
 import { loginAPI } from "../../api/auth";
 import { useAuthContext } from "../../contexts/auth";
-import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const { setAuth } = useAuthContext();
@@ -60,25 +61,23 @@ const Login = () => {
   return (
     <div className="flex justify-center items-center flex-col h-screen bg-gray-100">
       <form className="w-full p-5 shadow rounded sm:max-w-sm bg-white" onSubmit={onSubmit}>
-        <H1 className="mb-5 text-center" color="gray-700" size="2xl" weight="light">
+        <H1 className="mb-5 text-center text-gray-700" weight="normal">
           Bienvenido
         </H1>
 
-        <FormGroup label="Email:" size="sm" color="gray-700">
-          <Input type="text" name="email" placeholder="Ingresa tu Email..." color="gray-700" onChange={onChange} />
+        <FormGroup className="text-gray-700">
+          <Label>Email:</Label>
+
+          <Input type="text" name="email" placeholder="Ingresa tu Email..." onChange={onChange} />
         </FormGroup>
 
-        <FormGroup label="Contraseña:" size="sm" color="gray-700" className="mt-5">
-          <Input
-            type="password"
-            name="password"
-            placeholder="Ingresa tu contraseña..."
-            color="gray-700"
-            onChange={onChange}
-          />
+        <FormGroup className="text-gray-700">
+          <Label>Contraseña:</Label>
+
+          <Input type="password" name="password" placeholder="Ingresa tu contraseña..." onChange={onChange} />
         </FormGroup>
 
-        <div className="mt-5 text-right">
+        <div className="text-right">
           <Link className="text-sm text-blue-500" to="/">
             ¿Perdiste tu contraseña?
           </Link>
@@ -88,7 +87,7 @@ const Login = () => {
           <hr className="mt-5 border border-gray-100" />
         </div>
 
-        <Button type="submit" bg="blue-500" color="white" className="mt-5 relative">
+        <Button className="mt-5 relative" type="submit">
           Ingresar
           <MdKeyboardArrowRight className="absolute right-2 top-2.5 text-lg" />
         </Button>
