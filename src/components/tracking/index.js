c5511bf03a50c03288c98c1d6923a6c4682cd275
Ronald Abu Saleh <lancerrnld@gmail.com>
2022-07-22 19:28:08 -0400

    Shipments section advances + refresh button

--- src/components/tracking/index.js
+++ src/components/tracking/index.js
@@ -1,13 +1,17 @@
-import { H3 } from "../ui"
-import { useState, useEffect, memo } from "react"
+import { useState, useEffect } from "react"
+import { H2 } from "../ui"
 import { Marker } from "@react-google-maps/api"
 import { Map } from "../map"
+import Detail from "./Detail"
+
 import classNames from "classnames"
 import toast, { Toaster } from "react-hot-toast";
+
 import { getActiveShipmentDriversAPI, getShipmentTrackingCoordinatesAPI } from "../../api/shipments"
 
 const Tracking = () => {
   const [shipments, setShipments] = useState({ all: [], selected: null })
+  const [isDetailOpened, setIsDetailOpened] = useState(false)
 
   const notify = (type, message) => toast[type](message);
 
@@ -28,7 +32,7 @@ const Tracking = () => {
 
       interval = setInterval(() => {
         getTrackingCoordinates()
-      }, 10000)
+      }, 30000)
     }
 
     return () => {
@@ -43,7 +47,7 @@ const Tracking = () => {
       const data = response.data
 
       if (data.length) {
-        setShipments((state) => ({ ...state, all: data, selected: { shipmentId: data[0].shipmentId } }))
+        setShipments((state) => ({ ...state, all: data, selected: data[0] }))
       }
     }
   }
@@ -64,73 +68,99 @@ const Tracking = () => {
   }
 
   const selectShipment = (shipmentId) => {
-    setShipments((state) => ({ ...state, selected: { shipmentId } }))
+    const shipment = shipments.all.find(shipment => shipment.shipmentId === shipmentId)
+    setShipments((state) => ({ ...state, selected: shipment }))
+  }
+
+  const openDetail = (shipmentId) => {
+    const shipment = shipments.all.find(shipment => shipment.shipmentId === shipmentId)
+    
+    setShipments((state) => ({ ...state, selected: shipment }))
+    setIsDetailOpened(!isDetailOpened)
+  }
+
+  const closeDetail = () => {
+    setIsDetailOpened(!isDetailOpened)
+  }
+
+  const refresh = () => {
+    getActiveShipmentDrivers()
   }
 
   return (
-    <div>
+    <>
       <Toaster />
 
-      <Map style={{ height: "500px" }}>
-        {shipments.selected && (
-          <Marker position={{ lat: Number(shipments.selected.driverLatitude), lng: Number(shipments.selected.driverLongitude) }} />
-        )}
-      </Map>
+      {isDetailOpened && <Detail data={shipments.selected} onClose={closeDetail} />}
 
-      <H3 className="my-5 text-gray-700" weight="normal">Lista de envíos activos</H3>
+      {!isDetailOpened && (
+        <div>
+          <div className="mb-5 mt-5 text-right">
+            <button className="bg-blue-500 rounded py-2.5 px-10 text-white text-sm" onClick={refresh}>Actualizar</button>
+          </div>
 
-      <div className="whitespace-nowrap overflow-x-auto">
-        <table
-          className="w-full border-2 border-gray-100"
-          style={{ minWidth: "768px", borderCollapse: "collapse", borderSpacing: "0" }}
-        >
-          <thead>
-            <tr className="text-left border-2 border-gray-100">
-              <th className="p-3.5">#</th>
-              <th className="p-3.5">Nombre completo</th>
-              <th className="p-3.5">Cédula</th>
-              <th className="p-3.5">Licencia del vehículo</th>
-              <th className="p-3.5">Marca</th>
-              <th className="p-3.5">Modelo</th>
-              <th className="p-3.5">Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {shipments.all.map((data, index) => {
-              const isButtonDisabled = data.shipmentId === shipments.selected?.shipmentId
-              const buttonClasses = classNames("text-white text-sm rounded p-2", {
-                "bg-green-500": data.shipmentId !== shipments.selected?.shipmentId,
-                "bg-gray-400":  data.shipmentId === shipments.selected?.shipmentId
-              })
-              const buttonText = data.shipmentId === shipments.selected?.shipmentId ? "Seleccionado" : "Seleccionar"
-              
-              return (
-                <tr className="border-2 border-gray-100" key={data.shipmentId}>
-                  <td className="p-3.5">{index + 1}</td>
-                  <td className="p-3.5">
-                    <div className="flex items-center">
-                      <img className="w-10 h-10 rounded-full object-cover" src={data.driverPhoto} alt={data.driverName} />
-                      <span className="ml-2">{data.driverName} {data.driverLastname}</span>
-                    </div>
-                  </td>
-                  <td className="p-3.5">{data.driverIdentificationCode}</td>
-                  <td className="p-3.5">{data.vehicleLicenseNumber}</td>
-                  <td className="p-3.5">{data.vehicleBrand}</td>
-                  <td className="p-3.5">{data.vehicleModel}</td>
-                  <td className="p-3.5">
-                    <div className="flex gap-2">
-                      <button className={buttonClasses} disabled={isButtonDisabled} onClick={() => selectShipment(data.shipmentId)} style={{ minWidth: "98.8px" }}>{buttonText}</button>
-                      <button className="bg-blue-500 text-white text-sm rounded py-2 px-3" style={{ minWidth: "98.8px" }}>Detalle</button>
-                    </div>
-                  </td>
+          <Map style={{ height: "500px" }}>
+            {shipments.selected && (
+              <Marker position={{ lat: Number(shipments.selected.driverLatitude), lng: Number(shipments.selected.driverLongitude) }} />
+            )}
+          </Map>
+
+          <H2 className="my-5 text-gray-700" weight="normal">Lista de envíos</H2>
+
+          <div className="whitespace-nowrap overflow-x-auto">
+            <table
+              className="w-full border-2 border-gray-100"
+              style={{ minWidth: "768px", borderCollapse: "collapse", borderSpacing: "0" }}
+            >
+              <thead>
+                <tr className="text-left border-2 border-gray-100">
+                  <th className="p-3.5">#</th>
+                  <th className="p-3.5">Nombre completo</th>
+                  <th className="p-3.5">Cédula</th>
+                  <th className="p-3.5">Licencia del vehículo</th>
+                  <th className="p-3.5">Marca</th>
+                  <th className="p-3.5">Modelo</th>
+                  <th className="p-3.5">Acciones</th>
                 </tr>
-              )
-            })}
-          </tbody>
-        </table>
-      </div>
-    </div>
+              </thead>
+              <tbody>
+                {shipments.all.map((data, index) => {
+                  const isButtonDisabled = data.shipmentId === shipments.selected?.shipmentId
+                  const buttonClasses = classNames("text-white text-sm rounded p-2", {
+                    "bg-green-500": data.shipmentId !== shipments.selected?.shipmentId,
+                    "bg-gray-400":  data.shipmentId === shipments.selected?.shipmentId
+                  })
+                  const buttonText = data.shipmentId === shipments.selected?.shipmentId ? "Seleccionado" : "Seleccionar"
+                  
+                  return (
+                    <tr className="border-2 border-gray-100" key={data.shipmentId}>
+                      <td className="p-3.5">{index + 1}</td>
+                      <td className="p-3.5">
+                        <div className="flex items-center">
+                          <img className="w-10 h-10 rounded-full object-cover" src={data.driverPhoto} alt={data.driverName} />
+                          <span className="ml-2">{data.driverName} {data.driverLastname}</span>
+                        </div>
+                      </td>
+                      <td className="p-3.5">{data.driverIdentificationCode}</td>
+                      <td className="p-3.5">{data.vehicleLicenseNumber}</td>
+                      <td className="p-3.5">{data.vehicleBrand}</td>
+                      <td className="p-3.5">{data.vehicleModel}</td>
+                      <td className="p-3.5">
+                        <div className="flex gap-2">
+                          <button className={buttonClasses} disabled={isButtonDisabled} onClick={() => selectShipment(data.shipmentId)} style={{ minWidth: "98.8px" }}>{buttonText}</button>
+                          <button className="bg-blue-500 text-white text-sm rounded py-2 px-3" style={{ minWidth: "98.8px" }} onClick={() => openDetail(data.shipmentId)}>Detalle</button>
+                        </div>
+                      </td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
-export default memo(() => <Tracking />)
+export default Tracking
