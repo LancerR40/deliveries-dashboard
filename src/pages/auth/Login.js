bcd53589e8fd2c2df29183fa6f1599fc8755c581
Ronald Abu Saleh <lancerrnld@gmail.com>
2022-07-23 17:56:13 -0400

    Login notifications msg added

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,7 +1,8 @@
 import { Input, Button, FormGroup, Label, H1 } from "../../components/ui";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
+import toast, { Toaster } from "react-hot-toast";
 import { MdKeyboardArrowRight } from "react-icons/md";
 
 import { loginAPI } from "../../api/auth";
@@ -13,33 +14,35 @@ const Login = () => {
 
   const [data, setData] = useState({});
 
+  const notify = (type, message) => toast[type](message);
+
   const onSubmit = async (e) => {
     e.preventDefault();
 
     if (!data.email) {
-      return alert("Email requerido.");
+      return notify("error", "Debes ingresar un email para continuar.");
     }
 
     if (!data.password) {
-      return alert("La contraseña es requerida");
+      return notify("error", "Debes ingresar una contraseña para continuar.");
     }
 
     if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(data.email)) {
-      return alert("El email no es válido.");
+      return notify("error", "El email ingresado no es válido.");
     }
 
     if (data.password.length < 6) {
-      return alert("La contraseña debe tener seis(6) o más carácteres");
+      return notify("error", "La contraseña debe tener seis (6) o más carácteres.");
     }
 
     const response = await loginAPI(data);
 
     if (!response) {
-      return alert("Ups! ocurrio un error.");
+      return notify("error", "Ocurrió un error. Por favor, intenta más tarde.");
     }
 
     if (!response.success) {
-      return alert(response.error.message);
+      return notify("error", response.error.message);
     }
 
     if (response.success) {
@@ -60,6 +63,8 @@ const Login = () => {
 
   return (
     <div className="flex justify-center items-center flex-col h-screen bg-gray-100">
+      <Toaster />
+
       <form className="w-full p-5 shadow rounded sm:max-w-sm bg-white" onSubmit={onSubmit}>
         <H1 className="mb-5 text-center text-gray-700" weight="normal">
           Bienvenido
