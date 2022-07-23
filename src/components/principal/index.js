03510484f7aeb99b5b59c43b3aa289f067500e80
Ronald Abu Saleh <lancerrnld@gmail.com>
2022-07-23 13:33:53 -0400

    Principal section advances

--- src/components/principal/index.js
+++ src/components/principal/index.js
@@ -1,14 +1,61 @@
+import { useState, useEffect } from "react"
 import { H2 } from "../ui";
-import Map from "../map/Map";
+import { Map } from "../map";
+import { Marker } from "@react-google-maps/api"
+import toast, { Toaster } from "react-hot-toast";
+import { getAllShipmentTrackingCoordinatesAPI } from "../../api/shipments"
 
 const Principal = () => {
+  const [shipments, setShipments] = useState([])
+
+  const notify = (type, message) => toast[type](message);
+
+  useEffect(() => {
+    let interval = null
+
+    if (!interval) {
+      getAllShipmentTrackingCoordinates()
+
+      interval = setInterval(() => {
+        getAllShipmentTrackingCoordinates()
+      }, 20000)
+    }
+
+    return () => {
+      if (interval) {
+        clearInterval(interval)
+      }
+    }
+  }, [])
+
+  const getAllShipmentTrackingCoordinates = async () => {
+    const response = await getAllShipmentTrackingCoordinatesAPI()
+
+    if (response.success) {
+
+      if (!response.data.length) {
+        notify("error", "Aún no hay envíos activos.");
+      }
+
+      setShipments(response.data)
+    }
+  }
+
   return (
     <div>
+      <Toaster />
+
       <H2 className="mb-5 text-gray-700" weight="normal">
-        Pedidos activos
+        Envíos activos
       </H2>
 
-      <Map />
+      <Map>
+        {shipments.length > 0 && (
+          shipments.map((shipment) => (
+            <Marker key={shipment.driverIdentificationCode} label={`${shipment.driverName} ${shipment.driverLastname}`} position={{ lat: Number(shipment.driverLatitude), lng: Number(shipment.driverLongitude) }} />
+          ))
+        )}
+      </Map>
     </div>
   );
 };
